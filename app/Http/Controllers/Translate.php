11774b256e17dda08e4598d4e63a1ff45fbf5abb
Viet TD <tdviet.dev@gmail.com>
2018-07-20 01:05:00 +0700

    finish

--- app/Http/Controllers/Translate.php
+++ app/Http/Controllers/Translate.php
@@ -39,9 +39,10 @@ class Translate extends Controller
             $mListSentence = array_filter(explode($dot, $value));
             $list_sentence = array();
             foreach ($mListSentence as $key1 => $value1) {
-
-                $command = "python $file_segment '$value1' ";
-                $mstr = shell_exec($command);
+                if($input == "ja"){
+                    $command = "python $file_segment '$value1' ";
+                    $mstr = shell_exec($command);
+                } else  $mstr = $value1;
                 $list_sentence[] = $mstr;
             }
             $list_paragraph[] = $list_sentence;
