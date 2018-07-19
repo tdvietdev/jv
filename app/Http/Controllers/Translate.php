5984c5f7d31e11c8a8bdbe3b5a855830a05cd60f
Viet TD <tdviet.dev@gmail.com>
2018-07-19 07:36:22 +0700

    fix dot

--- app/Http/Controllers/Translate.php
+++ app/Http/Controllers/Translate.php
@@ -25,16 +25,18 @@ class Translate extends Controller
         if ($input == 'ja') {
             $transport_server = Config::get('configserver.japan_vietnam');
             $file_segment = Config::get('configserver.file_segment_jp');
+            $dot = "。";
         } else if ($input == 'vi') {
             $transport_server = Config::get('configserver.vietnam_japan');
             $file_segment = Config::get('configserver.file_segment_vn');
+            $dot = ".";
         }
         $transport = new Transport($transport_server);
         $client = new Client($transport);
         $list_paragraph = array();
         $mListParagraph = explode("\n", $text);
         foreach ($mListParagraph as $key => $value) {
-            $mListSentence = array_filter(explode("。", $value));
+            $mListSentence = array_filter(explode($dot, $value));
             $list_sentence = array();
             foreach ($mListSentence as $key1 => $value1) {
 
--- config/configserver.php
+++ config/configserver.php
@@ -4,7 +4,7 @@ return  [
  
     'recaptcha' => "6LdbikQUAAAAAL9dYPiA7-enRujr-Twa0N9-6Vmu",
     'japan_vietnam' => "http://103.92.28.246:8080/RPC2",
-    'vietnam_japan' => "",
+    'vietnam_japan' => "http://103.92.28.246:8888/RPC2",
     'file_segment_jp' => public_path("py/")."JP_Segmenter.py",
     'file_segment_vn' => public_path("py/")."VN_Segmenter.py",
     'captcha' => "no",
